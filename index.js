86f1828dc125bd6eab75c69549794a5873c31913
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2022-11-27 21:11:56 +0300

    restructured the files

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const applemojis = require('./applemojis.js');
+const applemojis = require('./Util/applemojis.js');
 
+// get all applemojis
 const getAll = () => {
     return applemojis;
 }
-
-const getOne = (code) => {
+// search by code
+const getOneByCode = (code) => {
     console.log('getting emoji with code=>', code);
+}
+// search by Short Name
+const getOneByShortName = (shortName) => {
+    console.log('getting emoji with Short Name=>', shortName);
 }
 
 
 
 
 
-
-module.exports = {getAll, getOne};
+module.exports = {getAll, getOneByCode};
